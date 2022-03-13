2785dae528db588d9e450b015c6b170cab0765d8
Gustavo Luigi <gustavoluigisouza@gmail.com>
2022-03-13 14:26:41 -0300

    .

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <link rel="stylesheet" type="text/css" href="assets/css/pages.css">
         <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
         <script src="https://cdn.jsdelivr.net/npm/vue?new=1"></script>
-        <script src="https://unpkg.com/vue-router@4.0.5/dist/vue-router.global.js?new=1"></script>
+        <script src="https://unpkg.com/vue-router@4.0.14/dist/vue-router.global.js?new=1"></script>
         <script src="https://cdn.jsdelivr.net/npm/vue-resource@1.5.1?new=1"></script>
     </head>
     <body>
